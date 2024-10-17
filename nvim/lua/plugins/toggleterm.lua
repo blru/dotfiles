46b3828d4c314cc4233f958606b9833cfe8b8fd6
Blerru <me@blerru.com>
2024-10-16 22:57:42 -0400

    perf(toggleterm): made toggleterm lazy load

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,25 +1,24 @@
 local keymaps = require("config.keymaps")
 
 return {
-    {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        config = function()
-            local toggleterm = require("toggleterm")
-
-            toggleterm.setup({
-                direction = "horizontal",
-                size = 12,
-                shade_terminals = false,
-                open_mapping = keymaps.terminal.toggle_visible,
-            })
-
-            vim.keymap.set(
-                "n",
-                keymaps.terminal.show_terminals,
-                "<cmd>TermSelect<CR>",
-                { desc = "Opens the terminal selection menu." }
-            )
-        end,
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = {
+        {
+            keymaps.terminal.show_terminals,
+            "<cmd>TermSelect<CR>",
+            desc = "Opens the terminal selection menu.",
+        },
+        { keymaps.terminal.toggle_visible, desc = "Toggles the terminal's visibility." },
     },
+    config = function()
+        local toggleterm = require("toggleterm")
+
+        toggleterm.setup({
+            direction = "horizontal",
+            size = 12,
+            shade_terminals = false,
+            open_mapping = keymaps.terminal.toggle_visible,
+        })
+    end,
 }
