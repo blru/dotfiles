3e2a8ac31f33612b514029a9d0fa152bd6bf5d36
Blerru <me@blerru.com>
2024-11-20 20:57:21 -0500

    fix(nvim/todo-comments): made todo comments highlight even before telescope key for finding them is pressed

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -2,6 +2,7 @@ local keymaps = require("config.keymaps")
 
 return {
     "folke/todo-comments.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
     keys = {
         {
