efc54d7ab9670bfd5aedd1e86c87116434bf9756
Blerru <44450511+blru@users.noreply.github.com>
2025-01-25 12:26:01 -0500

    impr: tweaked colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,25 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        require("tokyodark").setup({})
+        require("tokyodark").setup({
+            transparent_background = false, -- set background to transparent
+            gamma = 1.0, -- adjust the brightness of the theme
+            styles = {
+                comments = { italic = false }, -- style for comments
+                keywords = { italic = true }, -- style for keywords
+                identifiers = { italic = false }, -- style for identifiers
+                functions = {}, -- style for functions
+                variables = {}, -- style for variables
+            },
+            custom_highlights = {} or function(highlights, palette)
+                return {}
+            end, -- extend highlights
+            custom_palette = {} or function(palette)
+                return {}
+            end, -- extend palette
+            terminal_colors = true, -- enable terminal colors
+        })
+
         vim.cmd(":colorscheme tokyodark")
     end,
 }
