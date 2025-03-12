e28976b500a0bb80d2aff641094d66e150028a7a
Blerru <44450511+blru@users.noreply.github.com>
2025-03-12 19:15:25 -0400

    impr(nvim): decreased status bar refresh time and improved path module

--- .config/nvim/lua/plugins/status-line.lua
+++ .config/nvim/lua/plugins/status-line.lua
@@ -18,15 +18,24 @@ return {
                 always_divide_middle = true,
                 globalstatus = true,
                 refresh = {
-                    statusline = 1000,
+                    statusline = 200,
                     tabline = 1000,
                     winbar = 1000,
                 },
             },
             sections = {
                 lualine_a = { "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { "%{expand('%:~:.')}" }, -- relative file path, from https://stackoverflow.com/a/45244610 (I have literally no idea how this works)
+                lualine_b = {
+                    "branch",
+                    "diff",
+                    "diagnostics",
+                },
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                },
                 lualine_x = { "encoding", "filetype" },
                 lualine_y = { "progress" },
                 lualine_z = { "location" },
